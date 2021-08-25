c8a9dae71db22a2552b9dab49e65191834c348b0
Dillon Mulroy <dillon.mulroy@formidable.com>
2021-08-24 22:47:20 -0400

    init commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const splitwise = require('./splitwise');
 const getClient = require('./postgres');
+const ynab = require('./ynab');
 
 (async () => {
   const pg = await getClient();
   const expenses = await splitwise.getExpenses();
   const rows = await pg.getTransactions();
+  const budgets = await ynab.budgets.getBudgets();
 
-  console.log({ expenses, rows });
+  console.log(JSON.stringify({ expenses, rows, budgets }, null, 2));
   await pg.disconnect();
 })();
