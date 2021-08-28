c450c87eae971ab3e296a2d51bb0cb882fdcff46
Dillon Mulroy <dillon.mulroy@formidable.com>
2021-08-28 19:51:32 -0400

    add empty array

--- src/ynab.js
+++ src/ynab.js
@@ -3,6 +3,10 @@ const ynab = require('ynab');
 const client = new ynab.API(process.env.YNAB_API_TOKEN);
 
 async function createTransactions(transactions) {
+  if (transactions.length === 0) {
+    return [];
+  }
+
   const { data } = await client.transactions.createTransactions(
     process.env.YNAB_BUDGET_ID,
     {
