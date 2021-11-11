ddeb19e8ad7642e7a9f8e68c25ca251269d0fa1c
Dillon Mulroy <dillon.mulroy@formidable.com>
2021-11-11 15:14:16 -0500

    attempt at fixing js decimal math/rounding again

--- src/currency/conversions.ts
+++ src/currency/conversions.ts
@@ -1,19 +1,23 @@
 export function centsToDollars(amount: number): number {
-  return amount / 100;
+  return round(amount / 100);
 }
 
 export function centsToMiliunits(amount: number): number {
-  return amount * 10;
+  return round(amount * 10);
 }
 
 export function dollarsToCents(amount: number): number {
-  return amount * 100;
+  return round(amount * 100);
 }
 
 export function miliunitsToCents(amount: number): number {
-  return amount / 10;
+  return round(amount / 10);
 }
 
 export function dollarsToMiliunits(amount: number): number {
-  return amount * 1000;
+  return round(amount * 1000);
+}
+
+function round(number: number): number {
+  return Math.round((number + Number.EPSILON) * 100) / 100;
 }
