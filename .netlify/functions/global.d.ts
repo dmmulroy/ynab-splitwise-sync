3483d1446f9078e4d544a833e4a9ef92fea6a9f4
Dillon Mulroy <dillon@mainstreet.com>
2022-11-19 19:56:16 -0500

    add netlify function

---
+++ .netlify/functions/global.d.ts
@@ -0,0 +1,16 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      DATABASE_URL: string;
+      SPLITWISE_API_TOKEN: string;
+      SPLITWISE_GROUP_ID: string;
+      SPLITWISE_USER_ID: string;
+      YNAB_API_TOKEN: string;
+      YNAB_UNCATEGORIZED_ID: string;
+      YNAB_SPLITWISE_ACCOUNT_ID: string;
+      YNAB_BUDGET_ID: string;
+      YNAB_PAYEE_NAME?: string;
+    }
+  }
+}
+export {};
