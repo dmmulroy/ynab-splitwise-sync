b9120fb7da7a57c62a54f7808bc3d64077be51bf
Dillon Mulroy <dillon@mainstreet.com>
2022-11-25 22:52:21 -0500

    update script

--- scripts/sync.ts
+++ scripts/sync.ts
@@ -1,17 +1,33 @@
+import { appendFileSync } from 'fs';
+import { join } from 'path';
 import { SyncClient } from '../src/sync';
 
 (async () => {
-  const client = new SyncClient({
-    databaseUrl: process.env.DATABASE_URL,
-    splitwiseApiKey: process.env.SPLITWISE_API_TOKEN,
-    splitwiseGroupId: Number(process.env.SPLITWISE_GROUP_ID),
-    splitwiseUserId: Number(process.env.SPLITWISE_USER_ID),
-    ynabApiKey: process.env.YNAB_API_TOKEN,
-    ynabBudgetId: process.env.YNAB_BUDGET_ID,
-    ynabSplitwiseAccountId: process.env.YNAB_SPLITWISE_ACCOUNT_ID,
-    ynabUncategorizedCategoryId: process.env.YNAB_UNCATEGORIZED_ID,
-    ynabPayeeName: process.env.YNAB_PAYEE_NAME,
-  });
+  try {
+    const client = new SyncClient({
+      databaseUrl: process.env.DATABASE_URL,
+      splitwiseApiKey: process.env.SPLITWISE_API_TOKEN,
+      splitwiseGroupId: Number(process.env.SPLITWISE_GROUP_ID),
+      splitwiseUserId: Number(process.env.SPLITWISE_USER_ID),
+      ynabApiKey: process.env.YNAB_API_TOKEN,
+      ynabBudgetId: process.env.YNAB_BUDGET_ID,
+      ynabSplitwiseAccountId: process.env.YNAB_SPLITWISE_ACCOUNT_ID,
+      ynabUncategorizedCategoryId: process.env.YNAB_UNCATEGORIZED_ID,
+      ynabPayeeName: process.env.YNAB_PAYEE_NAME,
+    });
 
-  await client.sync();
+    await client.sync();
+
+    appendFileSync(
+      join(__dirname, '..', '..', 'ynab-sync-log.txt'),
+      `${new Date().toISOString()} | Sync Successful\n`,
+    );
+  } catch (error) {
+    appendFileSync(
+      join(__dirname, '..', '..', 'ynab-sync-log.txt'),
+      `${new Date().toISOString()} | Sync unsuccessful - ${JSON.stringify(
+        error,
+      )}\n`,
+    );
+  }
 })();
